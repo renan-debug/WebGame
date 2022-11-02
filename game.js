42b06c2233e332bb74cad9cd528ee645bf9e3df4
ramonglobinho <64386812+ramonglobinho@users.noreply.github.com>
2022-11-02 16:20:34 -0300

    atualizações com efeitos de mouse

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
+let mensagem = document.querySelector(".mensagem") ;
 
+function showMessage(){   
+   mensagem.style.display = "block"; 
+ }
+function hideMessage(){
+ mensagem.style.display = "none"; 
+ //document.getElementById("haddit").innerHTML = "Had! A manifestação de Nuit.";
+}
+
+function mostra(){
+    document.write("Had! A manifestação de Nuit");
+
+}
