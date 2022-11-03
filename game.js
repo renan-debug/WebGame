539c51ee2d4a6918a622c6e9e03ffaaed2e4073c
renan-debug <62522050+renan-debug@users.noreply.github.com>
2022-11-02 21:05:19 -0300

    Update game.js

--- game.js
+++ game.js
@@ -4,25 +4,31 @@ let crow3 = document.getElementById("3");
 
 
 function showMessage(){   
-   mensagem.style.display = "block"; 
+   //mensagem.style.display = "block";
+   mensagem.innerHTML ="Had! A manifestação de Nuit.";
    
  }
 function hideMessage(){
- mensagem.style.display = "none"; 
+ //mensagem.style.display = "none";
+   mensagem.innerHTML ="";
 }
 
 function mostra(){   
-  crow.style.display = "block"; 
+  //crow.style.display = "block"; 
+   mensagem.innerHTML ="O desvelar da companhia do céu.";
   
 }
 function apaga(){
-crow.style.display = "none"; 
+//crow.style.display = "none"; 
+   mensagem.innerHTML ="";
 }
 
 function mostra3(){   
-  crow3.style.display = "block"; 
+  //crow3.style.display = "block"; 
+   mensagem.innerHTML ="Todo Homem e toda mulher é uma estrela";
   
 }
 function apaga3(){
-crow3.style.display = "none"; 
+//crow3.style.display = "none"; 
+   mensagem.innerHTML ="";
 }
